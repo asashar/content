047ec146b715d73be6bdee7739c13325defb5e7a
Sammi Rosser <samanthajaderosser@gmail.com>
2025-01-17 08:53:24 +0000

    Add docstring and addtional comments to patient class

--- SurgeryPatient.py
+++ SurgeryPatient.py
@@ -1,33 +1,29 @@
-# A class representing patients referred to Neurosurgery
-# The patient's RTT clock tends to stop on admission for surgery
-
-import random
-
 class Patient:
-    def __init__(self, p_id, 
-                 already_seen_clinic=False,
-                 needs_surgery=False, 
-                 before_end_sim=True, 
-                 from_prefills=False
-                 ):
-        
+    """
+    A class representing patients referred to Neurosurgery
+
+    The patient's RTT clock tends to stop on admission for surgery
+    """
+    def __init__(self, p_id):
+
         self.id = p_id
         self.needs_surgery = False
 
         self.time_entered_pathway = 0
-        
+
         self.clinic_queue_time = 0
         self.theatre_queue_time = 0
         self.overall_queue_time = 0
 
- 
-        #attributes to help with pre filling queues
-        self.already_seen_clinic = False
-
-        #attribute for before/after end of sim
-        self.before_end_sim = True
-
-        #attribute for patients who are pre-filled into queues
+        # Attribute for patients who are pre-filled into queues
         self.from_prefills = False
 
+        # Attributes to help with pre filling queues
+        self.already_seen_clinic = False
 
+        # Attribute for whether a patient is added to the waiting list before or after
+        # the end of the simulation.
+        # The simulation continues to run after the 'end time' of the simulation - the
+        # requested simulation time just controls how long we keep adding patients into the
+        # simulation who we want to keep track of
+        self.before_end_sim = True
