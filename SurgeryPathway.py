5a03c697ad839a15d93a19186e62e638e9506377
Sammi Rosser <samanthajaderosser@gmail.com>
2025-01-27 15:00:54 +0000

    Enhance documentation of prefill and swap order
    By swapping order the patients moving through the queues have a more logical ID number so debugging becomes easier - i.e. by putting those who have already had their surgical clinic appt through this method first, the first person who then goes on to have their theatre slot will have an ID of 1, whereas existing code meant they had an ID of self.fill_non_admitted_queue+1, which was unintuitive

--- SurgeryPathway.py
+++ SurgeryPathway.py
@@ -238,37 +238,13 @@ class Neurosurgery_Pathway:
     def prefill_queues(self):
         """
         Method to pre fill queues with set numbers that are defined in the model class
+
+        First fills the patients who have already had their appointments with the surgical clinic
+        This ensures they get the earliest/lowest patient IDs
+
+        Next, prefill with those patients who have not yet had their surgical clinic appointments
+        so will need to proceed through the whole pathway
         """
-
-        log.debug(f"Prefilling non-admitted queues with {self.fill_non_admitted_queue} patients")
-
-        # Fill non-admitted queue
-        for i in range(self.fill_non_admitted_queue):
-
-            # Increment patient counter by 1
-            self.patient_counter += 1
-            self.active_entities += 1
-
-            # Create new patient
-            pt = Patient(self.patient_counter)
-            pt.from_prefills = True
-            self.event_log.append(
-                {'patient': self.patient_counter, 'event_type': 'arrival_departure',
-                 'event': 'arrival', 'time': self.env.now,
-                 'prefill': pt.from_prefills, 'prefill_already_seen_clinic': pt.already_seen_clinic,
-                 'before_end_sim': pt.before_end_sim, 'surgery_required': pt.needs_surgery
-                 }
-            )
-            # NOTE: the default value for new patients is that they have not
-            # already been seen by the clinic
-            # So these patients in the non-admitted queue have
-            # the sttribute *already_seen_clinic*=False
-
-            # Get simpy env to run enter_pathway method with this patient
-            self.env.process(self.enter_pathway(pt))
-
-            # need to have yield statement so code works - timeout for zero time
-            yield self.env.timeout(0)
 
         log.debug(f"Prefilling admitted queues with {self.fill_admitted_queue} patients")
 
@@ -300,6 +276,36 @@ class Neurosurgery_Pathway:
             self.env.process(self.enter_pathway(pt))
 
             # Need to have yield statement so code works - timeout for zero time
+            yield self.env.timeout(0)
+
+        log.debug(f"Prefilling non-admitted queues with {self.fill_non_admitted_queue} patients")
+
+        # Fill non-admitted queue
+        for i in range(self.fill_non_admitted_queue):
+
+            # Increment patient counter by 1
+            self.patient_counter += 1
+            self.active_entities += 1
+
+            # Create new patient
+            pt = Patient(self.patient_counter)
+            pt.from_prefills = True
+            self.event_log.append(
+                {'patient': self.patient_counter, 'event_type': 'arrival_departure',
+                 'event': 'arrival', 'time': self.env.now,
+                 'prefill': pt.from_prefills, 'prefill_already_seen_clinic': pt.already_seen_clinic,
+                 'before_end_sim': pt.before_end_sim, 'surgery_required': pt.needs_surgery
+                 }
+            )
+            # NOTE: the default value for new patients is that they have not
+            # already been seen by the clinic
+            # So these patients in the non-admitted queue have
+            # the sttribute *already_seen_clinic*=False
+
+            # Get simpy env to run enter_pathway method with this patient
+            self.env.process(self.enter_pathway(pt))
+
+            # need to have yield statement so code works - timeout for zero time
             yield self.env.timeout(0)
 
     def generate_referrals(self):
